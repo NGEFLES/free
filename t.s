51909cf6b89f5c4654ea181eec0872b196a7a01b
NGEFLES <135068429+NGEFLES@users.noreply.github.com>
2023-10-27 15:51:29 +0700

    Update t.s

--- t.s
+++ t.s
@@ -1,5 +1,13 @@
+#EXTINF:0 tvg-logo="https://static.wikia.nocookie.net/logo-tv/images/f/f2/Indosiar_%282012%29.png/revision/latest/scale-to-width-down/240?cb=20230414074513" group-title="----Local",3 Indosiar
+#https://app-etslive-2.vidio.com/live/205/master.m3u8
+#http://edge.linknetott.swiftserve.com/channelgroup3/cg331production/ch8/index.m3u8
+#http://210.210.155.35/session/8fe637d4-fb26-11eb-9071-b82a72d63267/qwr9ew/s/s04/01.m3u8
+#http://edge.linknetott.swiftserve.com/channelgroup3/cg331production/ch8/02.m3u8
+#http://tvnid.x10.mx/vidio/master.m3u8
+#https://cdn09jtedge.indihometv.com/dvr/139/indosiar/manifest.mpd
+#https://iptvme.xyz/vid/hls.m3u8?id=205
+#http://beenet.cloud/in.m3u8
+http://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=ts&play_token=GVZS088tcf
+http://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=mpd&play_token=GVZS088tcf
+http://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=m3u8&play_token=GVZS088tcf
 
-#EXTINF:-1 tvg-id="" tvg-name="ID: 01. ETV Live Event" tvg-logo="http://s3.i3ns.net/cs/etc/blank-icon.png" group-title="Indonesia",ID: 01. ETV Live Event
-https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=m3u8&play_token=GVZS088tcf
-https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=ts&play_token=GVZS088tcf
-https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=mpd&play_token=GVZS088tcf
