42b58fea817c8f36dcefa0bd177df90ab39656ba
NGEFLES <135068429+NGEFLES@users.noreply.github.com>
2023-10-27 15:43:37 +0700

    Update t.s

--- t.s
+++ t.s
@@ -1,6 +1,4 @@
-#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
-#KODIPROP:inputstream.adaptive.license_key=https://mrpw.ptmnc01.verspective.net/?deviceId=NDIzMDJhZmUtYWRjMi0zNGJkLTkyN2EtYmE1ZDFlZWIwODEz
-#EXTVLCOPT:http-referrer=https://www.vidio.com/live/205-indosiar
+
 #EXTINF:-1 tvg-id="" tvg-name="ID: 01. ETV Live Event" tvg-logo="http://s3.i3ns.net/cs/etc/blank-icon.png" group-title="Indonesia",ID: 01. ETV Live Event
 https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=m3u8&play_token=GVZS088tcf
 https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=ts&play_token=GVZS088tcf
