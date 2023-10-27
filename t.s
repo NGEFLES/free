aaf10917c48d1a723d6073c0efd006c3d1123149
NGEFLES <135068429+NGEFLES@users.noreply.github.com>
2023-10-27 15:43:14 +0700

    Update t.s

--- t.s
+++ t.s
@@ -3,3 +3,5 @@
 #EXTVLCOPT:http-referrer=https://www.vidio.com/live/205-indosiar
 #EXTINF:-1 tvg-id="" tvg-name="ID: 01. ETV Live Event" tvg-logo="http://s3.i3ns.net/cs/etc/blank-icon.png" group-title="Indonesia",ID: 01. ETV Live Event
 https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=m3u8&play_token=GVZS088tcf
+https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=ts&play_token=GVZS088tcf
+https://tv.trexiptv.com/play/live.php?mac=00:1a:79:00:10:10&stream=994885&extension=mpd&play_token=GVZS088tcf
